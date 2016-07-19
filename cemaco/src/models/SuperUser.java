1b80cbfcd097c46ca6405aa3541fcc5641145736
PabloLopez10 <lop14509@uvg.edu.gt>
2016-07-19 15:30:47 -0600

    Add files via upload
    Commit de models (superUser, Juguetes, Proveedor)

---
+++ cemaco/src/models/SuperUser.java
@@ -0,0 +1,33 @@
+package models;
+
+public class SuperUser {
+	
+	private String tipo;
+	private int restriccion;
+	
+	/**
+	 * @return the tipo
+	 */
+	public String getTipo() {
+		return tipo;
+	}
+	/**
+	 * @param tipo the tipo to set
+	 */
+	public void setTipo(String tipo) {
+		this.tipo = tipo;
+	}
+	/**
+	 * @return the restriccion
+	 */
+	public int getRestriccion() {
+		return restriccion;
+	}
+	/**
+	 * @param restriccion the restriccion to set
+	 */
+	public void setRestriccion(int restriccion) {
+		this.restriccion = restriccion;
+	}
+	
+}
